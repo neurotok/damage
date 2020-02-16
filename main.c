f699a84afc0dec5d87a1a37ef9f1fb00bfd5f98d
neurotok <marcin@pingpongcat.com>
2020-02-16 22:47:54 +0100

    EGL context creation attribute

--- main.c
+++ main.c
@@ -104,8 +104,10 @@ int main(void)
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
 
+
     glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint( GLFW_CLIENT_API, GLFW_OPENGL_ES_API );
+	glfwWindowHint( GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API );
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 2 );
 
 	window = glfwCreateWindow(800, 400, "Window Title", NULL, NULL);
